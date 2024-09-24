3babde37765d5fd85dc52c7ddc9cded09c032f05
arfanroky-github <arfanroky0@gmail.com>
2024-09-25 00:59:07 +0600

    refactor: responsive navbar and remove comment from root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import About from "@/components/About";
-import Experiences from "@/components/Experiences";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Services from "@/components/Services";
@@ -10,7 +8,7 @@ import { navItems } from "@/data";
 
 export default function HomePage() {
   return (
-    <div className="relative bg-black min-h-screen bg-zinc-900">
+    <div className="relative bg-black min-h-screen bg-zinc-900 overflow-hidden">
       <FloatingNav navItems={navItems} />
       <Hero />
       <About />
