fe81275a3b5bf012c07ffab606386556f1327b77
arfanroky-github <arfanroky0@gmail.com>
2024-10-18 19:34:34 +0600

    feat: add preloader when page is first render

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -4,10 +4,40 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { ReactLenis } from "lenis/react";
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+import { LottiePlayer } from "@/components/ui/lottie-player";
+import { AnimatePresence } from "framer-motion";
+
+export function Provider({ children, ...props }: ThemeProviderProps) {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    // Simulate some async task
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <NextThemesProvider {...props}>
-      <ReactLenis root>{children}</ReactLenis>
-    </NextThemesProvider>
+    <>
+      <NextThemesProvider {...props}>
+        <ReactLenis root>
+          <AnimatePresence>
+            {isLoading ? (
+              <div className="bg-zinc-900  flex justify-center items-center h-svh">
+                <LottiePlayer
+                  src={"/lottie-files/loader.json"}
+                  style={{
+                    width: "450px",
+                    height: "450px",
+                  }}
+                />
+              </div>
+            ) : (
+              children
+            )}
+          </AnimatePresence>
+        </ReactLenis>
+      </NextThemesProvider>
+    </>
   );
 }
