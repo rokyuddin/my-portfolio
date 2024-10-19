c29e7b4d470a4a54a6856188cc686bd05e917276
arfanroky-github <arfanroky0@gmail.com>
2024-10-19 15:17:55 +0600

    refactor: modified robots.ts file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,18 +2,11 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: [
-      {
-        userAgent: "*",
-        allow: "/",
-        disallow: ["/private/", "/admin/"],
-      },
-      {
-        userAgent: "Googlebot",
-        allow: "/",
-        disallow: "/nogooglebot/",
-      },
-    ],
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
+    },
     sitemap: "https://arfanroky.cloud/sitemap.xml",
     host: "https://arfanroky.cloud",
   };
