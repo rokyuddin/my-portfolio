de7c8f123b871cf3ca741ae2bd89c669c26affc5
arfanroky <arfanroky0@gmail.com>
2025-07-29 15:30:29 +0600

    feat: add responsive breakpoints to container configuration in Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,13 @@ const config: Config = {
       },
       container: {
         center: true,
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        },
         padding: {
           DEFAULT: "1rem",
           sm: "2rem",
